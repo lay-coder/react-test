238885e57e49ff28ad32b3e2ded9f576cdba2b07
nanmucoffin <898468959@qq.com>
2019-08-01 17:49:46 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import 'element-theme-default'
+import { Button } from 'element-react'
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import 'App.scss'
 import LayoutShow from 'views/show/layout-show/index'
@@ -8,8 +10,12 @@ export default class APP extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Link to="/show">show</Link>
-        <Link to="/console">console</Link>
+        <Button type="primary">
+          <Link to="/show">show</Link>
+        </Button>
+        <Button type="primary">
+          <Link to="/console">console</Link>
+        </Button>
         <Switch>
           <Route path="/show" component={LayoutShow} />
           <Route path="/console" component={LayoutConsole} />
